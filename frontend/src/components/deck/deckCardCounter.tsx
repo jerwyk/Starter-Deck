2049f3abc24847f22652bc202e89bc72faad18c8
jerwyk <Jerwyk0526@gmail.com>
2021-06-10 18:09:06 -0400

    updated deck counter

--- frontend/src/components/deck/deckCardCounter.tsx
+++ frontend/src/components/deck/deckCardCounter.tsx
@@ -1,14 +1,21 @@
 import { Fab } from "@material-ui/core";
-import React from "react";
+import React, { useMemo } from "react";
+import { Card } from "src/mobx/cardModel";
 interface IDeckDataProps {
-	deck?: any[];
+	deck?: Card[];
 }
 
 export default function CardCounter({ deck }: IDeckDataProps) {
+	const count = useMemo(() => {
+		let card_num = 0;
+		deck?.forEach((card) => (card_num += card.quantity));
+		return card_num;
+	}, [deck]);
+
 	return (
 		<div className="deck-counter">
 			<Fab color="primary" aria-label="add">
-				{deck?.length || 0}
+				{count}
 			</Fab>
 		</div>
 	);
