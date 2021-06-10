a5f9b36330a768b6e7c4d025503f1059111b2ff3
jerwyk <Jerwyk0526@gmail.com>
2021-06-10 18:09:20 -0400

    switch to functional component

--- frontend/src/components/editor/deckEditor.tsx
+++ frontend/src/components/editor/deckEditor.tsx
@@ -1,15 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import DeckView from "../deck/deckView";
 import EditorSidebar from "./editorSidebar";
 import "./styles.scss";
 
-export default class DeckEditor extends Component {
-	render() {
-		return (
-			<div className="editor-container">
-				<EditorSidebar />
-				<DeckView />
-			</div>
-		);
-	}
+export default function DeckEditor() {
+	return (
+		<div className="editor-container">
+			<EditorSidebar />
+			<DeckView />
+		</div>
+	);
 }
